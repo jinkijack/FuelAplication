0ace2fb911c3bbd4df5069570c06c8656aa9e929
tiago <tiagomc123@hotmail.com>
2023-05-25 09:17:40 -0300

    inicialização das variávies e rename

--- app/src/main/java/br/edu/ifsuldeminas/fuel/MainActivity.java
+++ app/src/main/java/br/edu/ifsuldeminas/fuel/MainActivity.java
@@ -15,13 +15,20 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText textEtanol;
     private TextInputEditText textGasolina;
     private Button calcular;
-    private ImageView melhorOpcao;
-    private TextView melhorOpcaotxt;
+    private ImageView imageFuel;
+    private TextView fuelMessage;
     private ImageView share;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textGasolina.findViewById(R.id.textInputEditTextGas);
+        textEtanol.findViewById(R.id.textInputEditTextEtanol);
+        calcular.findViewById(R.id.buttonCalcular);
+        imageFuel.findViewById(R.id.imageViewFuel);
+        fuelMessage.findViewById(R.id.textViewMessage);
+        share.findViewById(R.id.imageViewShare);
     }
 }
